f11d46565ebe0d32df3c8ad70b8018118902b6ed
정해준 <99087502+haejunejung@users.noreply.github.com>
2024-07-28 12:20:03 +0900

    Update forEach benchmark

--- benchmarks/performance/forEach.bench.ts
+++ benchmarks/performance/forEach.bench.ts
@@ -5,29 +5,29 @@ import { forEach as forEachUnderscore } from 'underscore';
 import { largeArray, noop, smallArray } from './_internal';
 
 describe('forEach, small array', () => {
-  bench('guardia/forEach, small array', () => {
+  bench('guardia/forEach', () => {
     forEachGuaria(smallArray, noop);
   });
 
-  bench('lodash/forEach, small array', () => {
+  bench('lodash/forEach', () => {
     forEachLodash(smallArray, noop);
   });
 
-  bench('underscore/forEach, small array', () => {
+  bench('underscore/forEach', () => {
     forEachUnderscore(smallArray, noop);
   });
 });
 
 describe('forEach, large array', () => {
-  bench('guardia/forEach, large array', () => {
+  bench('guardia/forEach', () => {
     forEachGuaria(largeArray, noop);
   });
 
-  bench('lodash/guardia, large array', () => {
+  bench('lodash/guardia', () => {
     forEachLodash(largeArray, noop);
   });
 
-  bench('underscore/guardia, large array', () => {
+  bench('underscore/guardia', () => {
     forEachUnderscore(largeArray, noop);
   });
 });
