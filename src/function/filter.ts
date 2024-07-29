0fa94adf18d06432fcc40f92981f6e380c828b48
haejunejung <jhj9422@unist.ac.kr>
2024-07-29 22:37:22 +0900

    📝 DOCS(filter)   : Add filter function docs

--- src/function/filter.ts
+++ src/function/filter.ts
@@ -1,3 +1,18 @@
+/**
+ * Returns the elements of an array that meet the condition specified in a predicate function.
+ *
+ * @template T - The type of elements in the input array.
+ * @template S - The type of elements in the output array.
+ * @param {ReadonlyArray<T>} array - The array to iterate over.
+ * @param {(value: T, index: number, array: ReadonlyArray<T>) => boolean} predicate
+ * A function that takes an element from the array; its index, and the array itself as arugments,
+ * and returns a boolean indicating whether the element should be included in the output array.
+ * @returns {ReadonlyArray<S>} - A new array that meet the condition specified in a `predicate` function.
+ * @example
+ * const words = ["a", "ab", "abc", "abcd", "abcde", "abcdef"];
+ * const overFourLengthWords = filter(words, word => word.length > 4); // ["abcde", "abcdef"]
+ */
+
 export const filter = <T, S extends T>(
   array: ReadonlyArray<T>,
   predicate: (value: T, index: number, array: ReadonlyArray<T>) => boolean
